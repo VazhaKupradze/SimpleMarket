84e0764a9f4321ed7d240ce8fa9bfbdaeafd753d
VazhaKupradze <vazha.kupradze@gmail.com>
2025-04-09 14:31:29 +0400

    init project

---
+++ SimpleMarket/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleMarket;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        // homeworkOne
+        int productPrice = 500;
+        float tax = productPrice * 18 / 100;
+        float totalPrice = tax + productPrice;
+        Console.WriteLine(totalPrice);
+    }
+}
