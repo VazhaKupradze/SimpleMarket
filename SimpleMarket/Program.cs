99768bc71a4a0cfec75ac2fca6cf3454ab7aa5ae
VazhaKupradze <Vazha.kupradze@gmail.com>
2025-04-28 22:20:13 +0400

    added product class

--- SimpleMarket/Program.cs
+++ SimpleMarket/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace SimpleMarket;
 
@@ -6,10 +8,85 @@ public static class Program
 {
     private static void Main(string[] args)
     {
-        // homeworkOne
-        int productPrice = 500;
-        float tax = productPrice * 18 / 100;
-        float totalPrice = tax + productPrice;
-        Console.WriteLine(totalPrice);
+
+        Product product = new Product();
+
+
+
+        Console.WriteLine("Enter product Name:");
+        var name = Console.ReadLine();
+        product.Name = name;
+
+        Console.WriteLine("Enter product Category:");
+        var category = Console.ReadLine();
+        product.Category = category;
+
+        Console.WriteLine("Enter product Price:");
+        var price = Console.ReadLine();
+        product.Price = decimal.Parse(price);
+
+
+        Console.WriteLine("Enter product Discount:");
+        var discount = Console.ReadLine();
+        product.Discount = decimal.Parse(discount);
+
+        Console.WriteLine("Enter product Quantity");
+        var quantity = Console.ReadLine();
+        product.Quantity = int.Parse(quantity);
+
+
+
+        product.CalculateFinalPrice();
+
+        Console.WriteLine("------------------------------");
+        product.DisplayInfo();
+
+        bool inStok = product.IsAvailable();
+
+        Console.WriteLine(inStok);
+    }
+
+}
+
+public class Product
+{
+    public string Name;
+    public string Category;
+    public decimal Price;
+    public decimal Discount;
+    public decimal DiscountedAmount;
+    public decimal FinalPrice;
+    public int Quantity;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Product: {Name}");
+        Console.WriteLine($"Category: {Category}");
+        Console.WriteLine($"Price: {Price}");
+        Console.WriteLine($"Discount: {Discount}");
+        Console.WriteLine($"Discounted Amount: {DiscountedAmount}");
+        Console.WriteLine($"Final Price: {FinalPrice}");
+
+
+    }
+
+    public void CalculateFinalPrice()
+    {
+        DiscountedAmount = (Price * Discount) / 100;
+        FinalPrice = Price - DiscountedAmount;
+    }
+
+    public bool IsAvailable()
+    {
+        if (Quantity > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
+
+
