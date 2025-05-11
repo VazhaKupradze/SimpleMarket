22af3c875c4aa79009cf0c1a1400f6ec7dd48fb4
VazhaKupradze <Vazha.kupradze@gmail.com>
2025-05-11 17:29:58 +0400

    refactored code using methods

---
+++ SimpleMarket/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SimpleMarket
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string Gender;
+        public int Age;
+        public string PersonalId;
+
+
+        public void DisplayInfo()
+        {
+
+            Console.WriteLine($"First Name: {FirstName}");
+            Console.WriteLine($"Last Name: {LastName}");
+            Console.WriteLine($"Gender: {Gender}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Personal ID: {PersonalId}");
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
--- SimpleMarket/Program.cs
+++ SimpleMarket/Program.cs
@@ -10,33 +10,7 @@ public static class Program
     {
         while (true)
         {
-
-            var product = new Product();
-
-            Console.WriteLine("Enter product name:");
-            var name = Console.ReadLine();
-            product.Name = name;
-
-
-            Console.WriteLine("Enter product category");
-            var category = Console.ReadLine();
-            product.Category = category;
-
-
-            Console.WriteLine("Enter product price");
-            var price = Console.ReadLine();
-            product.Price = decimal.Parse(price);
-
-
-            Console.WriteLine("Enter product discount");
-            var discount = Console.ReadLine();
-            product.Discount = decimal.Parse(discount);
-
-
-            Console.WriteLine("Enter product Quantity");
-            var quantity = Console.ReadLine();
-            product.Quantity = int.Parse(quantity);
-
+            var product = CreateProduct();
 
             DataBase.Products.Add(product);
 
@@ -54,7 +28,93 @@ public static class Program
             product.DisplayInfo();
         }
 
+        while (true)
+        {
+            var person = InitPerson();
+
+
+            DataBase.Persons.Add(person);
+
+            Console.WriteLine("Do you want to add another person?" +
+                "If Yes Press Y" +
+                "If no Press any bottom");
+            var secondAnswer = Console.ReadLine();
+            if (secondAnswer.ToUpper() != "Y")
+            {
+                break;
+            }
+        }
+
+
+        foreach (var person in DataBase.Persons)
+        {
+            person.DisplayInfo();
+        }
+
     }
+
+    public static Person InitPerson()
+    {
+        var person = new Person();
+
+        Console.WriteLine("Enter First Name:");
+        var firstName = Console.ReadLine();
+        person.FirstName = firstName;
+
+
+        Console.WriteLine("Enter Last Name:");
+        var lastName = Console.ReadLine();
+        person.LastName = lastName;
+
+
+        Console.WriteLine("Enter Gender:");
+        var gender = Console.ReadLine();
+        person.Gender = gender;
+
+
+        Console.WriteLine($"Enter {firstName}'s Age:");
+        var age = Console.ReadLine();
+        person.Age = int.Parse(age);
+
+        Console.WriteLine($"Enter {firstName}'s Personal ID:");
+        var personalId = Console.ReadLine();
+        person.PersonalId = personalId;
+
+        return person;
+    }
+
+    public static Product CreateProduct()
+    {
+        var product = new Product();
+
+        Console.WriteLine("Enter product name:");
+        var name = Console.ReadLine();
+        product.Name = name;
+
+
+        Console.WriteLine("Enter product category");
+        var category = Console.ReadLine();
+        product.Category = category;
+
+
+        Console.WriteLine("Enter product price");
+        var price = Console.ReadLine();
+        product.Price = decimal.Parse(price);
+
+
+        Console.WriteLine("Enter product discount");
+        var discount = Console.ReadLine();
+        product.Discount = decimal.Parse(discount);
+
+
+        Console.WriteLine("Enter product Quantity");
+        var quantity = Console.ReadLine();
+        product.Quantity = int.Parse(quantity);
+
+        return product;
+    }
+
+
 }
 
 
